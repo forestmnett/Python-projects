697c90f5797425b134d3ea297fa8968950d3552a
ForestNett <60995000+forestmnett@users.noreply.github.com>
2023-05-02 20:51:46 -0500

    Create Car_name_Generator.py

---
+++ Car_name_Generator.py
@@ -0,0 +1,6 @@
+print("This is a quick program to think of a nautical name for your car!")
+
+name1 = input("What is the color of your car?")
+name1 = name1 +"ish"
+name2 = input("What is a good nautical word? Ship, squid, shark, fish, pirate? It could be anything!")
+print(f"If you're brave enough, your cars name could be '{name1} {name2}'!")
