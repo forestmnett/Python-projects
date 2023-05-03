232a17cfe2c3e7ee6ac037fca35869c3fa0782c3
ForestNett <60995000+forestmnett@users.noreply.github.com>
2023-05-03 11:28:24 -0500

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,5 +1,5 @@
 #rock_paper_scissors.py
-
+import random
 rock = '''
     _______
 ---'   ____)
@@ -28,10 +28,37 @@ scissors = '''
 '''
 
 #Write your code below this line 👇
-import random
-
-x = print(input("What do you chose? Type 0 for Rock, 1 for Paper, or 2 for Scissors?"))
+x = input("What do you chose? Type 0 for Rock, 1 for Paper, or 2 for Scissors?")
 list=[rock, paper, scissors]
-print(list[])
+if x == "0":
+    print(rock)
+elif x == "1":
+    print(paper)
+elif x == "2":
+    print(scissors)    
+else:
+    print("invalid entry, Type 0 for Rock, 1 for Paper, or 2 for Scissors" )
+print("The computer chose:")
 computer_choice = random.choice(list)
 print(computer_choice)
+
+if x == "0" and computer_choice == rock:
+    print("Draw")
+elif x == "0" and computer_choice == paper:
+    print("You lose!")
+elif x == "0" and computer_choice == scissors:
+    print("You win!!")
+elif x == "1" and computer_choice == rock:
+    print("You win!!")
+elif x == "1" and computer_choice == paper:
+    print("Draw!")
+elif x == "1" and computer_choice == scissors:
+    print("You lose!!")
+elif x == "2" and computer_choice == rock:
+    print("You lose :( !!")
+elif x == "2" and computer_choice == paper:
+    print("You win :) !")
+elif x == "2" and computer_choice == scissors:
+    print("Draw")
+else:
+    print("something went wrong")
