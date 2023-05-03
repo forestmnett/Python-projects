60b5845314f71bc2becfab44f7978825e316197d
ForestNett <60995000+forestmnett@users.noreply.github.com>
2023-05-03 08:20:29 -0500

    Create Who_pays_the_bill.py

---
+++ Who_pays_the_bill.py
@@ -0,0 +1,15 @@
+# Import the random module here
+import random
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
+#Write your code below this line 👇
+print(names)
+x = len(names)
+#peeps = random.randint(0,x)
+random_person = random.randint(0, num_items - 1)
+print(peeps)
+#num = len(names)
+bill_pay = random.choice(names)
+print(f"{bill_pay} is going to but the meal today!")
