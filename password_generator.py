13deb0e5b1787c23f1b9576949cc52afa3188155
ForestNett <60995000+forestmnett@users.noreply.github.com>
2023-05-04 13:39:40 -0500

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,16 +1,29 @@
 #Password Generator Project
 import random
+from random import sample
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
-nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_letters = int(input("How many letters would you like in your password?\n")) 
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
+for letter in letters:
+    letter = random.choices(letters, k = nr_letters)
+for number in numbers:
+    number = random.choices(numbers, k = nr_numbers)
+for symbol in symbols:
+    symbol = random.choices(symbol, k = nr_symbols)
+print(symbol)
+password = letter + number + symbol
+password_list = random.shuffle(password)
+final_password = print(''.join(password))
+
+
 #Eazy Level - Order not randomised:
-#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+#e.g. 4 letter, 2 symbol, 2 number = JduE&!912
 
 
 #Hard Level - Order of characters randomised:
